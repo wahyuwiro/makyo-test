1040c471565193b1f5b480c86c023f07423ffd64
Wahyu <wahyu@usahakreatif.co.id>
2025-02-08 16:02:53 +0700

    Fix varcel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: "standalone",
+  experimental: {
+    serverActions: {},
+  },
 };
 
 export default nextConfig;
